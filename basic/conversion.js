e12d4c582a464a953c3f977f03d2bb50c114ff1b
roy17360 <133860839+roy17360@users.noreply.github.com>
2023-10-19 08:48:13 +0000

    conversion

---
+++ basic/conversion.js
@@ -0,0 +1,32 @@
+/*console.log("1">0)
+console.log("1"<0)
+console.log("1">=0)*/
+
+//console.log(null==0)
+//console.log(null>=0);//conversion into comparison // in undefined cases always false 
+
+//strict check
+/*console.log("1"===1);
+console.log("02">1);*/
+
+
+//operation
+/*let value=1
+let negativeValue=-value
+console.log(negativeValue);*/
+
+//
+/*console.log(1+"2");
+console.log("1"+2);
+console.log("1"+"2");
+//what if
+console.log("1"+2+1);//all conversion in string
+console.log(1+2+"1");// priorites of first number guide line
+*/
+
+
+
+//incriment
+let v=100
+++v;
+console.log(v);
