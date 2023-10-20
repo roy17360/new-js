14af04967cab6fa439c745e9df3f14641055b62c
roy17360 <133860839+roy17360@users.noreply.github.com>
2023-10-20 12:18:36 +0000

    arraysomenew

---
+++ basic1/array1.js
@@ -0,0 +1,28 @@
+/*const heros=["krish","shaktiman","leo"]
+const desi=["ram","dj","loki"]
+
+//heros.push(desi) //by push but its not systematic
+//console.log(heros);
+
+const newhero=heros.concat(desi) // by concat
+console.log(newhero);
+
+const newhee=[...heros,...desi] //spread
+console.log(newhee);
+*/
+/*
+const another_array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]]
+
+const real_another_array = another_array.flat(Infinity) //it igone all braces and give value
+console.log(real_another_array);
+*/
+/*
+console.log(Array.isArray("Hitesh")) // check array of string present or not
+console.log(Array.from("Hitesh")) // divide each word into strin
+console.log(Array.from({name: "hitesh"})) // interesting
+*/
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3)); // it enter value into array
